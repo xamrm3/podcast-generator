ea6592752f889bc2deeb02862d58c51fc3bf9754
xamrm3 <110181628+xamrm3@users.noreply.github.com>
2025-10-20 20:38:49 +0000

    fixed spelling error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "============="
 
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.eamil "${INPUT_EMAIL}"
+git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
